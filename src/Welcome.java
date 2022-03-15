887cba193488a140232bb9b7774bcf9102d7787f
khushi173 <khushijamwal173@gmail.com>
2022-03-15 14:39:15 +0530

    message

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,9 +1,13 @@
 public class Welcome {
 
     public static void main(String[] args) {
-       Integer a = 10, b = 10, c;
-       c=a+b;
-        System.out.println(c);
-    }
+            int x = 156, y = 89;
 
-}
+
+
+
+
+
+
+
+
